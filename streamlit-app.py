7b055f18d9eeba4c0ef1963f3e6f89a40806fd70
cpatterson71 <carl.pattersonsd@gmail.com>
2024-04-03 17:24:52 -0700

    UPdate to database.

--- streamlit-app.py
+++ streamlit-app.py
@@ -10,70 +10,75 @@ from st_aggrid import AgGrid
 #                   aws_access_key_id = 'AKIASCPKVVLRRR24MAEI',
 #                   aws_secret_access_key = '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'
 #                     )
-# s3.download_file('carldata', 'My_Equipment_file.xlsx', 'My_Equiptment_file.xlsx')
-# file = 'My_Equipment_File.xlsx'
+# s3.download_file('carldata', 'Equipment_and_Metrology_Database.xlsx', 'Equipment_and_Metrology_Database.xlsx')
+
+# file = 'Equpment_and_Metrology_Database.xlsx'
 # excel = pd.read_excel('Equipment_and_Metrology_Database.xlsx', index_col=False)
 # temp = pd.DataFrame(excel)
-
-
-Search_Box = ('ID #', 'Location', 'Type', 'Serial #', 'Description', 'Cal_Date',
-            'Cal_Due_Date', 'Owner', 'Comment')
 
 st.set_page_config(layout='wide')
 st.title('Equipment and Metrology Database')
 
-file = 'Equipment_and_Metrology_Database.xlsx'
-data = pd.read_excel(file, index_col=False)
 
+# temp = temp.loc[:,['ID_No', 'Location', 'Type', 'Serial_No', 'Description', 'Cal_Date',
+#             'Cal_Due_Date', 'Owner', 'Comment' ]]
+
+# tests = temp['Cal_Date'].astype(str)
+# dates = []
+# for test in tests:
+#      dates.append(test[:-9])
+# temp['Cal_Date'] = dates
+
+# boys = temp['Cal_Due_Date'].astype(str)
+# why = []
+# for boy in boys:
+#      why.append(boy[:-9])
+# temp['Cal_Due_Date'] = why
+
+
+# temp.to_csv('Equipment_and_Metrology_Database.csv', index=False)
+
+Search_Box = ('ID_No', 'Location', 'Type','Serial_No', 'Description', 'Cal_Date',
+            'Cal_Due_Date')
+
+file='Equipment_and_Metrology_Database.csv'
+data = pd.read_csv(file, index_col=False)
 temp = pd.DataFrame(data)
+temp = temp.astype('str')
 
-temp = temp.loc[:,['ID #', 'Location', 'Type', 'Serial #', 'Description', 'Cal_Date',
-            'Cal_Due_Date', 'Owner', 'Comment' ]]
+AgGrid(temp, fit_columns_on_grid_load=True)
 
-tests = temp['Cal_Date'].astype(str)
-dates = []
-for test in tests:
-     dates.append(test[:-9])
-temp['Cal_Date'] = dates
-
-date = temp['Cal_Due_Date'].astype(str)
-why = []
-for date in dates:
-     why.append(date[:-9])
-temp['Cal_Due_Date'] = why
-
-@st.cache
-def load_data():
-    df = temp
-    return df
+# @st.cache
+# def load_data():
+#     df = temp
+#     return df
 
 df = load_data
 
-def search(data, column, search_term):
-    if column == 'ID #':
-        search_term = (search_term)
 
-        indexes = data.loc[data[column].isin([search_term])].index
-        if indexes.size > 0:
-            return data.iloc[indexes]
-        else:
-            return []
+# def search(data, column, search_term):
+#     if column == 'ID_No':
+#         search_term = (search_term)
+
+#         indexes = data.loc[data[column].isin([search_term])].index
+#         if indexes.size > 0:
+#             return data.iloc[indexes]
+#         else:
+#             return []
         
-buffer, col2, col3 = st.columns([1, 20, 60])
+# buffer, col2, col3 = st.columns([1, 20, 60])
 
-with col2:
-    key = st.sidebar.selectbox("Key",Search_Box)
+# with col2:
+#     key = st.sidebar.selectbox("Key",Search_Box)
 
-with col3:
-    search_term = st.sidebar.text_input("Search")
-    if key != '' and search_term != '':
-        df = search(data, key, search_term)
+# with col3:
+#     search_term = st.sidebar.text_input("Search")
+#     if key != '' and search_term != '':
+#         df = search(data, key, search_term)
 
-buffer, col2 = st.columns([1, 100])
+# buffer, col2 = st.columns([1, 100])
 
-with col2:
-    if not df.empty:
-        AgGrid(df, height=500, editable=False, use_container_width=True)
-    else:
-        st.write('Did not find any material matching the criteria')
+# with col2:
+#         AgGrid(df, height=500, use_container_width=True)
+    
     
