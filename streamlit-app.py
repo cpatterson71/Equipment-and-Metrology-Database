def022d51f32142d333c4a1c91593eed51e8cb9b
cpatterson71 <carl.pattersonsd@gmail.com>
2024-04-04 15:26:41 -0700

    Included pull from S3 bucket

--- streamlit-app.py
+++ streamlit-app.py
@@ -4,39 +4,37 @@ import streamlit as st
 import numpy as np
 from st_aggrid import AgGrid
 
-# import boto3
+import boto3
 
-# s3 = boto3.client('s3',
-#                   aws_access_key_id = 'AKIASCPKVVLRRR24MAEI',
-#                   aws_secret_access_key = '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'
-#                     )
-# s3.download_file('carldata', 'Equipment_and_Metrology_Database.xlsx', 'Equipment_and_Metrology_Database.xlsx')
+s3 = boto3.client('s3',
+                  aws_access_key_id = 'AKIASCPKVVLRRR24MAEI',
+                  aws_secret_access_key = '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'
+                    )
+s3.download_file('carldata', 'Equipment_and_Metrology_Database.xlsx', 'Equipment_and_Metrology_Database.xlsx')
 
-# file = 'Equpment_and_Metrology_Database.xlsx'
-# excel = pd.read_excel('Equipment_and_Metrology_Database.xlsx', index_col=False)
-# temp = pd.DataFrame(excel)
+file = 'Equpment_and_Metrology_Database.xlsx'
+excel = pd.read_excel('Equipment_and_Metrology_Database.xlsx', index_col=False)
+temp = pd.DataFrame(excel)
+
+temp = temp.loc[:,['ID_No', 'Location', 'Type', 'Serial_No', 'Description', 'Cal_Date',
+            'Cal_Due_Date', 'Owner', 'Comment' ]]
+
+tests = temp['Cal_Date'].astype(str)
+dates = []
+for test in tests:
+     dates.append(test[:-9])
+temp['Cal_Date'] = dates
+
+boys = temp['Cal_Due_Date'].astype(str)
+why = []
+for boy in boys:
+     why.append(boy[:-9])
+temp['Cal_Due_Date'] = why
+
+temp.to_csv('Equipment_and_Metrology_Database.csv', index=False)
 
 st.set_page_config(layout='wide')
 st.title('Equipment and Metrology Database')
-
-
-# temp = temp.loc[:,['ID_No', 'Location', 'Type', 'Serial_No', 'Description', 'Cal_Date',
-#             'Cal_Due_Date', 'Owner', 'Comment' ]]
-
-# tests = temp['Cal_Date'].astype(str)
-# dates = []
-# for test in tests:
-#      dates.append(test[:-9])
-# temp['Cal_Date'] = dates
-
-# boys = temp['Cal_Due_Date'].astype(str)
-# why = []
-# for boy in boys:
-#      why.append(boy[:-9])
-# temp['Cal_Due_Date'] = why
-
-
-# temp.to_csv('Equipment_and_Metrology_Database.csv', index=False)
 
 Search_Box = ('ID_No', 'Location', 'Type','Serial_No', 'Description', 'Cal_Date',
             'Cal_Due_Date')
